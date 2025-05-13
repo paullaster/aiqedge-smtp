e1442f5978ff2399e09381316b2074f49efeed74
paullaster <paullaster>
2025-05-13 07:39:32 +0300

    lint: fix lint config and lint error

--- src/infrastructure/mail/smtpProvider.ts
+++ src/infrastructure/mail/smtpProvider.ts
@@ -7,14 +7,9 @@ export class SmtpProvider implements ISmtpProvider {
         if (!provider) throw new Error('Missing email provider');
         switch (provider) {
             case 'nodemailer': {
-                try {
-                    const nodeMilerProvider = new EmailTransportProvider();
-                    const info = await nodeMilerProvider.sendMail(email);
-                    return { success: true, message: 'Email sent via infrastructure SMTP', info };
-                } catch (error) {
-                    throw error;
-                }
-
+                const nodeMilerProvider = new EmailTransportProvider();
+                const info = await nodeMilerProvider.sendMail(email);
+                return { success: true, message: 'Email sent via infrastructure SMTP', info };
             }
         }
     }
