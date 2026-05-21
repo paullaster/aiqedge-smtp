0a5bc04c3f2a0d2bc8da37c7b14067c138ae9b7e
paullaster <paullasterokoth98@gmail.com>
2026-05-21 09:04:18 +0300

    ref: typecast

--- src/interfaces/controllers/smtpController.ts
+++ src/interfaces/controllers/smtpController.ts
@@ -19,7 +19,7 @@ class SmtpController {
 
     async sendEmail(req: Request, res: Response): Promise<void> {
         try {
-            const clientId = req.params.clientId;
+            const clientId = req.params.clientId as string;
             const emailData = req.body;
             const smtpconfig: SmtpConfigShape = req.smtpConfig;
             const result = await executeSendEmail(
